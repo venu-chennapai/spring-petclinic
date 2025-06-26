bae1967a7d43bdf1d857c9d9f390abef4d0c9357
venu chennapai <87475479+venu-chennapai@users.noreply.github.com>
2025-06-27 00:19:38 +0530

    Update PetClinicApplication.java
    Signed-off-by: venu chennapai <87475479+venu-chennapai@users.noreply.github.com>

--- src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
+++ src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
@@ -32,6 +32,7 @@ public class PetClinicApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PetClinicApplication.class, args);
+        System.Out.Println("Hai this is venu");
 	}
 
 }

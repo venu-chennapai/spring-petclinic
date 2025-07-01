13d8c834bbbe9e6e74032e5fd15766a086359b3b
venu chennapai <venu.chennapai@gmail.com>
2025-07-01 14:29:48 +0000

    This is sample_project_commit_1 by venu

--- src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
+++ src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
@@ -33,6 +33,7 @@ public class PetClinicApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PetClinicApplication.class, args);
 		System.out.println("Hello this is venu");
+		System.out.println("Hello this is venu gopal varma");
 	}
 
 }
